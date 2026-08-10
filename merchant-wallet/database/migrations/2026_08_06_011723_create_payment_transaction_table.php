79321608e17f20a35be2db3d5c6669acff99ad1f
tzebyng <tb.wong@nvx.protb.wong@nvx.pro>
2026-08-10 11:22:48 +0800

    fix migration bug

--- merchant-wallet/database/migrations/2026_08_06_011723_create_payment_transaction_table.php
+++ merchant-wallet/database/migrations/2026_08_06_011723_create_payment_transaction_table.php
@@ -33,6 +33,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('payment_transaction');
+        Schema::dropIfExists('payment_transactions');
     }
 };
